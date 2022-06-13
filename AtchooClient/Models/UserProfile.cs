3db063a9788791a21dd612385e0195e7183be1c0
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-06-13 16:29:38 -0700

    add properties for user profile
    Co-authored-by: Mark McConnell <mark.programming1@gmail.com>
    Co-authored-by: Meron G. Tekie <MeronTekie@users.noreply.github.com>
    Co-authored-by: Marcus Anthony Lorenzo <marcusanthonylorenzo@users.noreply.github.com>
    Co-Authored-by: Jake Edgar <jakeedgar1012@gmail.com>
    co-authored-by: Evgeniya Meshuris <em@theemm.com>

---
+++ AtchooClient/Models/UserProfile.cs
@@ -0,0 +1,11 @@
+namespace AtchooClient.Models
+{
+  public class UserProfile
+  {
+    public int UserProfileId { get; set; }
+    public string UserId { get; set; }
+    public string Name { get; set; }
+    public int DOB { get; set; }
+    public string Allergy { get; set; }
+  }
+}
