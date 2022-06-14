e9f59b86cc43aaca305d77a69a138392ad8155c9
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-06-14 10:28:52 -0700

    remove allergy and create many to many relationship for profile/allergies
    Co-authored-by: Mark McConnell <mark.programming1@gmail.com>
    Co-authored-by: Meron G. Tekie <MeronTekie@users.noreply.github.com>
    Co-authored-by: Marcus Anthony Lorenzo <marcusanthonylorenzo@users.noreply.github.com>
    Co-Authored-by: Jake Edgar <jakeedgar1012@gmail.com>
    co-authored-by: Evgeniya Meshuris <em@theemm.com>

--- AtchooClient/Models/UserProfile.cs
+++ AtchooClient/Models/UserProfile.cs
@@ -1,11 +1,23 @@
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
 namespace AtchooClient.Models
 {
   public class UserProfile
   {
+    public UserProfile()
+    {
+      this.JoinEntities = new HashSet<ProfileAllergy>();
+    }
+    
     public int UserProfileId { get; set; }
-    public string UserId { get; set; }
     public string Name { get; set; }
     public int DOB { get; set; }
-    public string Allergy { get; set; }
+    public string Bio { get; set; }
+    public string ProfileImg { get; set; }
+    public virtual ApplicationUser User { get; set; }
+    public virtual ICollection<ProfileAllergy> JoinEntities { get; }
   }
 }
