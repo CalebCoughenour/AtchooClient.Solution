0ac7612b478ed42df17070f566fff5dc3dfe24db
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-06-14 09:52:01 -0700

    comment out API data, make that a stretch goal
    Co-authored-by: Mark McConnell <mark.programming1@gmail.com>
    Co-authored-by: Meron G. Tekie <MeronTekie@users.noreply.github.com>
    Co-authored-by: Marcus Anthony Lorenzo <marcusanthonylorenzo@users.noreply.github.com>
    Co-Authored-by: Jake Edgar <jakeedgar1012@gmail.com>
    co-authored-by: Evgeniya Meshuris <em@theemm.com>

--- AtchooClient/Models/ApplicationUser.cs
+++ AtchooClient/Models/ApplicationUser.cs
@@ -1,37 +1,47 @@
 using Microsoft.AspNetCore.Identity;
-using AtchooClient.ViewModels;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using Microsoft.AspNetCore.Http;
-using System.Web;
-using System;
 
 namespace AtchooClient.Models
 {
-    public class ApplicationUser : IdentityUser
-    {
-        private static string _userId { get; set; }
-
-        public static void Register(RegisterViewModel model)
-        {
-            var apiCallTask = ApiHelper.ApiPostRegister(model);
-            var result = apiCallTask.Result;
-            JValue jsonResponse = JsonConvert.DeserializeObject<JValue>(result);
-            
-            _userId = jsonResponse.ToString();
-
-        }
-
-        public static string Login(LoginViewModel model)
-        {
-            var apiCallTask = ApiHelper.ApiPostLogin(model);
-            var result = apiCallTask.Result;
-            JValue jsonResponse = JsonConvert.DeserializeObject<JValue>(result);
-            
-            
-            return jsonResponse.ToString();
-        }
-
-
-    }
+  public class ApplicationUser : IdentityUser
+  {
+    
+  }
 }
+
+// using Microsoft.AspNetCore.Identity;
+// using AtchooClient.ViewModels;
+// using Newtonsoft.Json;
+// using Newtonsoft.Json.Linq;
+// using Microsoft.AspNetCore.Http;
+// using System.Web;
+// using System;
+
+// namespace AtchooClient.Models
+// {
+//     public class ApplicationUser : IdentityUser
+//     {
+//         private static string _userId { get; set; }
+
+//         public static void Register(RegisterViewModel model)
+//         {
+//             var apiCallTask = ApiHelper.ApiPostRegister(model);
+//             var result = apiCallTask.Result;
+//             JValue jsonResponse = JsonConvert.DeserializeObject<JValue>(result);
+            
+//             _userId = jsonResponse.ToString();
+
+//         }
+
+//         public static string Login(LoginViewModel model)
+//         {
+//             var apiCallTask = ApiHelper.ApiPostLogin(model);
+//             var result = apiCallTask.Result;
+//             JValue jsonResponse = JsonConvert.DeserializeObject<JValue>(result);
+            
+            
+//             return jsonResponse.ToString();
+//         }
+
+
+//     }
+// }
