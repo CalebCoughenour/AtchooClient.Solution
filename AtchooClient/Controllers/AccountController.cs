d53075a9d05c8fc12d327e03865093a994f524a8
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-06-14 17:29:22 -0700

    remove using directive
    Co-authored-by: Mark McConnell <mark.programming1@gmail.com>
    Co-authored-by: Meron G. Tekie <MeronTekie@users.noreply.github.com>
    Co-authored-by: Marcus Anthony Lorenzo <marcusanthonylorenzo@users.noreply.github.com>
    Co-Authored-by: Jake Edgar <jakeedgar1012@gmail.com>
    co-authored-by: Evgeniya Meshuris <em@theemm.com>

---
+++ AtchooClient/Controllers/AccountController.cs
@@ -0,0 +1,72 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AtchooClient.Models;
+using AtchooClient.ViewModels;
+using Microsoft.AspNetCore.Identity;
+
+namespace AtchooClient.Controllers
+{
+  public class AccountController : Controller
+    {
+        private readonly AtchooClientContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController (UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, AtchooClientContext db)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _db = db;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Register (RegisterViewModel model)
+        {
+            var user = new ApplicationUser { UserName = model.UserName, Email = model.Email };
+            IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Login(LoginViewModel model)
+        {
+            Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(model.UserName, model.Password, isPersistent: true, lockoutOnFailure: false);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+        [HttpPost]
+        public async Task<ActionResult> LogOff()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
+
