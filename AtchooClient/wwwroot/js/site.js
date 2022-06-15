96f61fc2760099dae0c0ac06a2b065fb4a034b31
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-06-15 16:00:22 -0700

    add allergy page ul style
    Co-authored-by: Meron G. Tekie <MeronTekie@users.noreply.github.com>
    Co-authored-by: Marcus Anthony Lorenzo <marcusanthonylorenzo@users.noreply.github.com>
    Co-Authored-by: Jake Edgar <jakeedgar1012@gmail.com>
    co-authored-by: Evgeniya Meshuris <em@theemm.com>
    Co-authored-by: Mark McConnell <mark.programming1@gmail.com>

--- AtchooClient/wwwroot/js/site.js
+++ AtchooClient/wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+const clickHandler = document.querySelector("#nav-link");
+const navlink = document.querySelector("#nav-link a.active");
+console.log(clickHandler), navLink
+
+
