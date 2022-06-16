a496286377ba371e694a70d89a6c66209ab2ca79
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-06-16 12:30:32 -0700

    restore email back to original settings
    Co-authored-by: Meron G. Tekie <MeronTekie@users.noreply.github.com>
    Co-authored-by: Marcus Anthony Lorenzo <marcusanthonylorenzo@users.noreply.github.com>
    Co-Authored-by: Jake Edgar <jakeedgar1012@gmail.com>
    co-authored-by: Evgeniya Meshuris <em@theemm.com>
    Co-authored-by: Mark McConnell <mark.programming1@gmail.com>

--- AtchooClient/ViewModels/RegisterViewModel.cs
+++ AtchooClient/ViewModels/RegisterViewModel.cs
@@ -1,12 +1,13 @@
 using System.ComponentModel.DataAnnotations;
 
+
 namespace AtchooClient.ViewModels
 {
   public class RegisterViewModel
   {
     [Required]
-    [EmailAddress]
     [Display(Name = "Email")]
+    [EmailAddress]
     public string Email { get; set; }
     
     [Required]
